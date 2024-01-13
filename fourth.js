51fefd434776dafa7799cac30fdaa081228b8f2c
M.Rizwan <rizwan02091986@gmail.com>
2024-01-13 20:51:00 +0500

    fourth

---
+++ fourth.js
@@ -0,0 +1,64 @@
+// for(let i=0; i<=10; i++){
+//     console.log("PJS", i);
+// }
+// // string inpolation
+// for(let i=0; i<=10; i++){
+//     console.log(`The value of i is  ${i}`);
+// }
+
+
+
+// table making
+// let number = prompt("enter the number");
+
+// for(let i=1; i<=10; i++){
+
+//     console.log(`${number} x ${i} = ${number*i}`);
+// }
+
+//white loop
+
+// let myNum = 80;
+// while(myNum<=84){
+//     console.log("this is while loop", myNum);
+//     myNum++;
+// }
+
+// let z = 44;
+// do{
+//     console.log("this is do while loop", z);
+//     z++;
+// }while (z<=50);
+
+// let gameNum = 95;
+// let userNum = prompt("Enter The NUmber");
+// while(userNum != gameNum){
+//     userNum = prompt("you guess the wrong number : you guess again");
+// }
+
+// swal("you win the game!")
+
+
+// for in loop
+
+let obj ={
+    name : "Rizwan",
+    age : 38,
+    grade : "A",
+    city : "Karachi"
+}
+
+// even and odd
+for(let n in obj){
+    console.log(n , obj[n]);
+}
+
+let num = prompt("Enter the number");
+
+for(let i=0; i<=num; i++){
+    if(i%2==0){
+        console.log(`${i} is even`);
+    }else{
+        console.log(`${i} is odd`);
+    }
+}
