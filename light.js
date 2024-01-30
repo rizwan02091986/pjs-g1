c4a67502fbf51653994e56b41f689c5ab072e3a9
muniba <munibaali0712@gmail.com>
2024-01-30 20:25:47 +0500

    bulb

---
+++ light.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById('btn');
+let bulb = document.getElementById('bulb');
+let mq = document.getElementById('mq');
+
+btn.addEventListener('click', toggleBulb);
+    function toggleBulb(){
+
+        if(btn.textContent.includes('On') && mq.textContent.includes('On')){
+            bulb.src = "./image/onbulb.png";
+            btn.textContent = "Turn Off";
+            mq.textContent = "Turn Off";
+            btn.className = "btn btn-outline-success";
+            btn.style.boxShadow = "3px 3px 4px 4px green";
+            mq.style.color = "green";
+            mq.style.boxShadow = "3px 3px 4px 4px green";
+            bulb.style.boxShadow = "3px 3px 4px 4px green";
+            mq.style.padding = "10px";
+        }
+        else{
+            bulb.src = "./image/bulb.png";
+            btn.textContent = "Turn On";
+            mq.textContent = "Turn On";
+            btn.className = "btn btn-outline-danger";
+            btn.style.boxShadow = "3px 3px 4px 4px red";
+            mq.style.color = "red";
+            mq.style.boxShadow = "3px 3px 4px 4px red";
+            bulb.style.boxShadow = "3px 3px 4px 4px red";
+            mq.style.padding = "10px";
+        }
+    }
