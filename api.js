c9682377eb83ab7512c77ba7b83e494b6ba74620
muniba <munibaali0712@gmail.com>
2024-02-06 20:11:42 +0500

    promise

--- api.js
+++ api.js
@@ -8,8 +8,34 @@
 //     console.log("Wow@@2!");
 // },2000)
 
-let promiseOne = new Promise(function(resole, reject){
+// primise 1
+let promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("This is First Promise!");
+    },3000)
+})
+
+// promise Two
+let promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({name : "Henrry", email : "global@gmail.com"})
     },5000)
+})
+
+promiseTwo.then(function(user){
+    console.log(user);
+})
+
+// Promise Three
+let promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username : "Henry", password : "12345"})
+    },6000)
+})
+
+promiseThree.then(function(MyUser){
+    return MyUser.password;
+})
+.then(function(pass){
+    console.log(pass);
 })
